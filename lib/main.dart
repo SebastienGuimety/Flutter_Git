1d912b0c9d518da3c44144eab032132003e7e4a3
sebastienguimety <seb.guimety@gmail.com>
2023-09-19 17:30:04 +0200

    new

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a blue toolbar. Then, without quitting the app,
